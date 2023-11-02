fef0423670aff830753d41af64157cbdc4c5de28
LeviathanTurtle <56966597+LeviathanTurtle@users.noreply.github.com>
2023-11-02 08:45:34 -0400

    updated twoPointOperations

--- twoPointOperations/gridpointOperations.cpp
+++ twoPointOperations/gridpointOperations.cpp
@@ -1,29 +1,44 @@
-/*
-   William Wadsworth
-   10/22/2020
-   CSC1710
-   ~/csc1710/lab10/wadsworthlab10.cpp
-   yay more functions awesome
+/* GRIDPOINT OPERATIONS
+ * William Wadsworth
+ * Created: 10.22.2020
+ * Dcotored: 11.2.2023
+ * CSC 1710
+ * ~/csc1710/lab10/wadsworthlab10.cpp
+ *
+ *
+ * [DESCRIPTION]:
+ * This function prompts the user for two coordinate points, and the operation
+ * the user would like to perform.
+ *
+ *
+ * [COMPILE/RUN]:
+ * To compile:
+ *     g++ gridpointOperations -Wall -o gridpointOperations
+ *
+ * To run:
+ *     ./gridpointOperations
 */
 
-// libraries because I'm lazy
+
 #include <iomanip>
 #include <cmath>
 #include <iostream>
 using namespace std;
 
+
 // global variable
 const double PI = 3.141592;
 
+
 // function prototypes
 double distance(double q, double w, double e, double r);
-double distance(double t, double y);
+//double distance(double t, double y);
 double radius(double a, double s, double d, double f);
-double radius(double g, double h);
+//double radius(double g, double h);
 double circumference(double r);
 double area(double r);
 
-// main program cool
+
 int main()
 {
    // local variables
@@ -47,7 +62,7 @@ int main()
    cout << "What operation would you like to do: ";
    cin >> inp;
 
-/* I don't understand why this doesn't work. Any ideas? Shouldn't be because I'm comparing a string right?
+   //I don't understand why this doesn't work. Any ideas? Shouldn't be because I'm comparing a string right?
 
    // input validation
    while (inp != "distance" || inp != "radius" || inp != "circumference" || inp != "area")
@@ -55,7 +70,7 @@ int main()
       cout << "Not valid: ";
       cin >> inp;
    }
-*/
+
 
    // call functions based on input
    if (inp == "distance")
@@ -66,42 +81,41 @@ int main()
       cout << "The circumference is " << circumference(radius(x1, y1, x2, y2)) << endl;
    else if (inp == "area")
       cout << "The area is " << area(radius(x1, y1, x2, y2)) << " units squared" << endl;
-   else
-   {
-      cout << "Not valid. Please choose: distance, radius, circumference, area: ";
-      cin >> inp;
+   else {
+      cerr << "Not valid. exiting...\n";
+      exit(1);
    }
 
    // terminate on success, scream at me if not
    return 0;
 }
 
-// haha funny functions
+
 // functions return calculated value depending on operation
 double distance(double q, double w, double e, double r)
 {
    return sqrt(pow(e - q, 2) + pow(r - w, 2));
 }
-
+/*
 double distance(double t, double y)
 {
    return sqrt(pow(t, 2) + pow(y, 2));
 }
-
+*/
 double radius(double a, double s, double d, double f)
 {
    double r = distance(a, s, d, f);
 
    return r;
 }
-
+/*
 double radius(double g, double h)
 {
    double r = distance(g, h);
 
    return r;
 }
-
+*/
 double circumference(double r = 1)
 {
    return 2 * PI * r;
