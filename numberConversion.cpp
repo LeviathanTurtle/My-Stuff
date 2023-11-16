35f5402b4ded3d018eac749a118e9e67ff484d3e
LeviathanTurtle <56966597+LeviathanTurtle@users.noreply.github.com>
2023-11-16 09:29:38 -0500

    first draft: numberConversion

---
+++ numberConversion.cpp
@@ -0,0 +1,189 @@
+/* CONVERTING INTEGERS TO STRINGS (WITH SCIENTIFIC NOTATION)
+ * William Wadsworth
+ * Created: 11.16.2023
+ * 
+ * 
+ * [DESCRIPTION]:
+ * This program converts integers to strings (and vice versa), accounting for
+ * scientific notation for numbers greater than 1,000. The number can be proved
+ * as input, passed as an argument in program execution, or a file of numbers
+ * can be provided.
+ * 
+ * 
+ * [COMPILE/RUN]:
+ * To compile:
+ *     g++ numberConversion.cpp -Wall -o numberConversion
+ * 
+ * To run (5 args, 4 optional):
+ *     ./numberConversion [-f] [file] [number] [string]
+ * 
+ * where:
+ *     [-f]     - optional flag, specify if you are providing a file
+ *     [file]   - optional, but required if -f is specified, file containing
+ *                numbers in or not in scientific notation to convert
+ *     [number] - optional, provide a number to convert
+ *     [string] - optional, provide a number in scientific notation
+ * 
+ * Note: if providing a second argument, it must either be a number or a file.
+ * 
+ * 
+ * [EXIT/TERMINATING CODES]:
+ * 0 - program successfully completed a full execution
+ * 
+ * 1 - CLI args used incorrectly
+ * 
+ * 2 - file not provided (-f is specified)
+ * 
+ * 3 - file unable to be opened
+*/
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+// FUNCTION PROTOTYPES
+// ----------------------------------------------------------------------------
+
+// function to process file, calls string or int conversion
+void processFile(const char*, const bool&);
+
+// function to search for e or E if the string (number in scientific notation)
+// is provided -- provided by ChatGPT
+bool searchCharacter(const char*);
+
+// function to convert integers to scientific notation
+string intToString(int, const bool&);
+// function to convert integers to scientific notation - from file
+string* intToString(ifstream&, const bool&);
+
+// function to convert numbers in scientific notation to ingegers
+int stringToInt(string, const bool&);
+// function to convert numbers in scientific notation to ingegers - from file
+int* stringToInt(istream&, const bool&);
+
+
+// MAIN CODE
+// ----------------------------------------------------------------------------
+int main(int argc, char* argv[])
+{
+    // check optional CLI args
+    // only checking upper bound - 1 is required, less than 1 arg is 0 args 
+    // provided which means nothing. Max of 3 args
+    if(argc > 3) {
+        cerr << "error: CLI args used incorrectly: ./numberConversion [-f] "
+             << "[file] [number] [string].\n\nwhere:\n"
+             << "    [-f]     - optional flag, specify if you are providing a file\n"
+             << "    [file]   - optional, but required if -f is specified, file\n"
+             << "               containing numbers in or not in scientific notation to convert\n"
+             << "    [number] - optional, provide a number to convert\n"
+             << "    [string] - optional, provide a number in scientific notation\n";
+        exit(1);
+    }
+
+
+    // if only the exe arg was provided, set a flag to prompt user for item to
+    // process
+    bool promptForItem = false;
+    if(argc == 1)
+        promptForItem = true;
+
+
+    // process command line
+    if(argv[1] == "-f") {
+        // -f specified, file MUST follow
+        
+        // call function for file, which calls its respective function
+        if(argc == 3)
+            processFile(argv[2]);
+        else {
+            cerr << "error: file not provided.\n";
+            exit(2);
+        }
+    }
+    else {
+        // no file is specified
+        
+        // is argv[1] a string or int?
+        // if string
+        //     stringToInt
+        // else
+        //     intToString
+        
+        // we know it's a string if e or E is in argv[1]
+        if(searchCharacter(argv[1]))
+            string number = stringToInt(argv[1],promptForItem); // contains e or E
+        else
+            int number = intToString(atoi(argv[1]),promptForItem);
+    }
+
+
+    return 0;
+}
+
+
+// function to process file, calls string or int conversion
+void processFile(const char* filename, const bool& promptForItem)
+{
+    ifstream file (filename);
+
+    if(!file) {
+        cerr << "error: file (" << filename << ") unable to be opened.\n";
+        exit(3);
+    }
+
+    // check first value in file
+    // if it contains e or E --> stringToInt
+    // else --> intToString
+    if(searchCharacter(file.peek()))
+        stringToInt(file,promptForItem);
+    else
+        intToString(file,promptForItem);
+
+
+    // close file
+    file.close();
+}
+
+
+// function to search for e or E if the string (number in scientific notation)
+// is provided -- provided by ChatGPT
+bool searchCharacter(const char* string)
+{
+    // iterate through string
+    for(char ch : string)
+        if(ch == 'e' || ch == 'E')
+            return true; // found e or E
+    
+    // e and E not found
+    return false;
+}
+
+
+// function to convert integers to scientific notation
+string intToString(int number, const bool& promptForItem)
+{
+
+}
+
+
+// function to convert integers to scientific notation
+// CONVERT FROM FILE
+string* intToString(ifstream& file, const bool& promptForItem)
+{
+
+}
+
+
+// function to convert numbers in scientific notation to ingegers
+int stringToInt(string number, const bool& promptForItem)
+{
+
+}
+
+
+// function to convert numbers in scientific notation to ingegers
+// CONVERT FROM FILE
+int* stringToInt(ifstream& file, const bool& promptForItem)
+{
+
+}
