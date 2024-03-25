e42ecc0a718b559303f8ebdc9464cf42e21e8bba
LeviathanTurtle <wbwads@gmail.com>
2024-03-25 14:16:21 -0400

    finished rust-ifying tempConv

--- Rust/tempConv.rs
+++ Rust/tempConv.rs
@@ -43,75 +43,85 @@ fn main() {
     }
 */
     println!("Do you want to run this program? [Y/n]: ");
+    // personal note: `mut` is required because for this way because we are defining a variable,
+    // then assigning it later
     let mut confirmation = char;
     io::stdin().read_line(&mut confirmation).expect("Failed to read response");
 
-
-
-
-}
-    
-    // display 3 decimal places in output
-    cout << fixed << showpoint << setprecision(3);
-
-    //=========================================================================
-    // SMALLEST DEGREE
-
-    // give instructions to define sdegree
+// --- SMALLLEST DEGREE -------------------------
+/*
     double sdegree;
     cout << "Give your starting (smallest) Fahrenheit degree [-1000 < degree <"
          << " 1000]: ";
     cin >> sdegree;
 
-    // input validation
     while (sdegree < -1000) {
         cout << "Not valid, degree must be > -1000: ";
         cin >> sdegree;
     }
+*/
+    // give instructions to define sdegree
+    println!("Give your starting (smallest) Fahrenheit degree [-1000 < this_degree < 1000]: ");
+    let mut smallest_degree = int;
+    io::stdin().read_line(&mut smallest_degree).expect("Failed to read degree");
 
-    //=========================================================================
-    // LARGEST DEGREE
+    // input validation
+    while smallest_degree <= -1000 and smallest_degree >= 1000 {
+        println!("Not valid, degree limitations: [-1000 < this_degree < 1000]: ");
+        io::stdin().read_line(&mut smallest_degree).expect("Failed to read degree");
+    }
 
-    // give instructions to define ldegree
+// --- LARGEST DEGREE ---------------------------
+/*
     double ldegree;
     cout << "Give your ending (largest) Fahrenheit degree [-1000 < degree < "
          << "1000]: ";
     cin >> ldegree;
 
-    // input validation
     while (ldegree < sdegree || ldegree > 1000) {
         cout << "Not valid, degree must be < 1000: ";
         cin >> ldegree;
     }
+*/
+    // give instructions to define ldegree
+    println!("Give your ending (largest) Fahrenheit degree [smallest_degree < this_degree < 1000]: ");
+    let mut largest_degree = int;
+    io::stdin().read_line(&mut largest_degree).expect("Failed to read degree");
 
-    //=========================================================================
-    // INCREMENT
+    // input validation
+    while largest_degree <= smallest_degree and largest_degree >= 1000 {
+        println!("Not valid, degree limitations: [smallest_degree < this_degree < 1000]: ");
+        io::stdin().read_line(&mut largest_degree).expect("Failed to read degree");
+    }
 
-    // define increment
+// --- INCREMENT --------------------------------
+/*
     double increment;
     cout << "How much do you want to increment by: ";
     cin >> increment;
 
-    // input validation
     while (increment <= 0) {
         cout <<"Not valid, increment must be > 0: ";
         cin >> increment;
     }
+*/
+    // define increment
+    println!("How much do you want to increment by: ");
+    let mut increment = float;
+    io::stdin().read_line(&mut increment).expect("Failed to read increment");
 
-    //=========================================================================
-    // TABLE + FORMULAS
+    // input validation
+    while increment <= 0 {
+        println!("Not valid, increment must be > 0: ");
+        io::stdin().read_line(&mut increment).expect("Failed to read increment");
+    }
 
-    // make table heading using spaces
+// --- TABLE AND FORMULAS -----------------------
+/*
     cout << " Fahrenheit (°F) |  Celsius (°C)  |   Kelvin (K)   " << endl;
     cout << "---------------------------------------------------" << endl;
 
-    // define celsius and kelvin formulae
-    //double c = ((sdegree - 32) * 5/9);
-    //double k = ((sdegree - 32) * 5/9 + 273.15);
-
-    // while loop to run through incremented degrees
     while (sdegree <= ldegree) {
-        // display calculations
         cout << setw(12) << sdegree << "     |" << setw(12)  
              << ((sdegree-32) * 5/9) << "    |" << setw(12) 
              << ((sdegree-32) * 5/9 + 273.15) << endl;
@@ -120,3 +130,20 @@ fn main() {
 
     return 0;
 }
+*/
+    // make table heading using spaces
+    println!(" Fahrenheit (°F) |  Celsius (°C)  |   Kelvin (K)   ");
+    println!("---------------------------------------------------");
+
+    // define celsius and kelvin formulae
+    //double c = ((sdegree - 32) * 5/9);
+    //double k = ((sdegree - 32) * 5/9 + 273.15);
+
+    // while loop to run through incremented degrees
+    while(smallest_degree  <= largest_degree) {
+        // display calculations
+        println!("{:>12.3}     |{:>12.3}    |{:>12.3}", smallest_degree, (smallest_degree - 32) * 5 / 9, (smallest_degree - 32) * 5 / 9 + 273.15);
+        smallest_degree += increment;
+    }
+}
+
