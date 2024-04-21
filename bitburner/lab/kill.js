545ae4acea1be1ef3a52c037f67091127681568d
LeviathanTurtle <wbwads@gmail.com>
2024-04-21 16:49:12 -0400

    bitburner now has its own repo

--- bitburner/lab/kill.js
+++
@@ -1,8 +0,0 @@
-/** @param {NS} ns */
-export async function main(ns, servers) {
-  for (let i = 0; i < servers.length; ++i) {
-    ns.killall(servers[i]);
-  }
-  ns.tprint("DONE -- stopping scripts");
-}
-
