8f344261e3e53f06a1525eb054cfbb4e0ae05ce2
LeviathanTurtle <wbwads@gmail.com>
2024-04-29 10:02:06 -0400

    rough layout for new program

---
+++ final_grade_calc.cpp
@@ -0,0 +1,8 @@
+// calculates final grade (similar to that website)
+
+// settings/presets stored in external file
+
+// list of options to add to final calc: test, quiz, lab, hw, etc
+// last option to add assignments, updating external files as necessary
+
+// maybe a table or some kind of structured output
