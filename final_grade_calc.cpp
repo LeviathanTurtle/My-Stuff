5a49487509d39400a5dda4ba5bb776dad5005f58
LeviathanTurtle <wbwads@gmail.com>
2024-05-07 11:37:41 -0400

    layout for final_grade_calc

--- final_grade_calc.cpp
+++ final_grade_calc.cpp
@@ -1,8 +1,49 @@
-// calculates final grade (similar to that website)
+/* FINAL GRADE CALCULATOR
+ * William Wadsworth
+ * Created: sometime
+ * 
+ * [DESCRIPTION]:
+ * This program calculates the final grade in a class. Assignment names and their weighted
+ * percentages are stored in an external file named ______. Default assignments are homework,
+ * quizzes, tests, and labs. The user has the option to add additional assignments with their
+ * weights or change an existing assignments weighted percentage, which will all be saved in the
+ * same config file. 
+ * TODO: maybe a table or some kind of optional structured output
+ * 
+ * [USAGE]:
+ * To compile:
+ *     g++ final_grade_calc.cpp -Wall -o <exe name>
+ * To run:
+ *     ./<exe name> <config file name> 
+ * 
+ * [EXIT/TERMINATING CODES]:
+ * 0 - program successfully completed full execution
+ * 
+ * 1 - 
+*/
 
-// settings/presets stored in external file
+#include <iostream>
+using namespace std;
 
-// list of options to add to final calc: test, quiz, lab, hw, etc
-// last option to add assignments, updating external files as necessary
 
-// maybe a table or some kind of structured output
+bool DEBUG = false;
+
+
+
+int main(int argc, char* argv[])
+{
+    // check CLI args
+
+
+    // call loadFile func
+    // BASED ON CLI ARGS
+
+
+    // prompt user for action
+    // switch/case
+    
+
+    // 
+
+    return 0;
+}
