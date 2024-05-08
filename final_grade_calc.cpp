a1b2363acb21bd809e88348e7d175de159c56dbc
LeviathanTurtle <wbwads@gmail.com>
2024-05-07 22:24:25 -0400

    continued work

--- final_grade_calc.cpp
+++ final_grade_calc.cpp
@@ -1,6 +1,6 @@
 /* FINAL GRADE CALCULATOR
  * William Wadsworth
- * Created: sometime
+ * Created: at some point
  * 
  * [DESCRIPTION]:
  * This program calculates the final grade in a class. Assignment names and their weighted
@@ -14,36 +14,93 @@
  * To compile:
  *     g++ final_grade_calc.cpp -Wall -o <exe name>
  * To run:
- *     ./<exe name> <config file name> 
+ *     ./<exe name> [-d] <config file name> 
+ * where:
+ * [-d] - optional, enable debug output
  * 
  * [EXIT/TERMINATING CODES]:
  * 0 - program successfully completed full execution
  * 
- * 1 - 
+ * 1 - invalid CLI args
+ * 
+ * 2 - 
 */
 
 #include <iostream>
+#include <fstream>      // file I/O
+#include <map>          // hold config options
 using namespace std;
 
 
 bool DEBUG = false;
 
 
+void loadFile(const char*);
+map<string, double> initMap();
+
+
+// the defaults are made up values
+//map<string, double> config;
+
 
 int main(int argc, char* argv[])
 {
     // check CLI args
+    if(argc != 3) {
+        cerr << "Error: invalid arguments. Usage: ./<exe name> [-d] <config file name> \n";
+        exit(1);
+    }
 
 
     // call loadFile func
     // BASED ON CLI ARGS
+    if (argv[1] == "-d") {
+        // debug
+        DEBUG = true;
+
+        loadFile(argv[2]);
+    }
+    else {
+        // not debug
+
+        loadFile(argv[1]);
+    }
+
+
+    // load map
 
 
     // prompt user for action
     // switch/case
-    
+
 
     // 
 
     return 0;
+}
+
+
+void loadFile(const char* filename)
+{
+    ifstream file (filename);
+
+
+
+    file.close();
+}
+
+
+map<string, double> initMap()
+{
+    map<string, double> config;
+
+    // defaults
+    config["homework"] = 0.1;
+    config["quiz"] = 0.15;
+    config["lab"] = 0.15;
+    config["project"] = 0.15;
+    config["test"] = 0.2;
+    config["exam"] = 0.25;
+
+    // load from file
 }
