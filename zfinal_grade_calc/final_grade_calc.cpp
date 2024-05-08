95810f439d47424734fb066e015ead8c8ce2fc1d
LeviathanTurtle <wbwads@gmail.com>
2024-05-08 17:22:29 -0400

    progress

--- zfinal_grade_calc/final_grade_calc.cpp
+++ zfinal_grade_calc/final_grade_calc.cpp
@@ -24,6 +24,8 @@
  * 1 - invalid CLI args
  * 
  * 2 - file unable to be opened
+ * 
+ * 3 - invalid operation choice
 */
 
 #include <iostream>
@@ -35,9 +37,12 @@ using namespace std;
 bool DEBUG = false;
 
 
-void loadFile(const char*);
+//map<string, double> loadFile(const char*);
 map<string, double> initMap(ifstream&);
-map<string, double> addMapItem(ifstream&);
+void printMap(const map<string, double>&);
+void action(map<string, double>&, const char*);
+map<string, double> addMapItem(map<string, double>&, const string&, const double&, const char*);
+map<string, double> editMapItem(map<string, double>&, const string&, const double&, const char*);
 
 
 // the defaults are made up values
@@ -59,16 +64,13 @@ int main(int argc, char* argv[])
         // debug
         DEBUG = true;
 
-        loadFile(argv[2]);
+        map<string, double> config = initMap(argv[2]);
     }
     else {
         // not debug
 
-        loadFile(argv[1]);
+        map<string, double> config = initMap(argv[1]);
     }
-
-
-    // load map
 
 
     // prompt user for action
@@ -81,34 +83,20 @@ int main(int argc, char* argv[])
 }
 
 
-void loadFile(const char* filename)
+// note: this function only initializes the map
+map<string, double> initMap(const char* filename)
 {
     if (DEBUG)
-        printf("Entering loadFile...\n");
-    
+        printf("Entering initMap...\n");
+
+    map<string, double> config;
+
     ifstream file (filename);
     // check file is opened
     if (!file) {
         cerr << "Error: file unable to be opened.\n";
         exit(2);
     }
-
-    initMap(file);
-
-    file.close();
-
-    if (DEBUG)
-        printf("Exiting loadFile...\n");
-}
-
-
-// note: this function only initializes the map
-map<string, double> initMap(ifstream& file)
-{
-    if (DEBUG)
-        printf("Entering initMap...\n");
-
-    map<string, double> config;
 
     // defaults
     /*
@@ -129,20 +117,113 @@ map<string, double> initMap(ifstream& file)
     // load from file
     string category;
     double weight;
-    //for (int i=0; i<config.size(); i++)
     while (file >> category >> weight)
         config[category] = weight;
 
+    file.close();
+
     if (DEBUG) {
-        for (const auto& entry : config)
-            //cout << entry.first << ": " << entry.second << "\n";
-            printf("Loaded map:\n%s: %.2f\n", entry.first.c_str(), entry.second);
+        printf("Loaded map:\n");
+        printMap(config);
         printf("\nExiting initMap...\n");
+    }
+
+    return config;
+}
+
+
+void printMap(const map<string, double>& config)
+{
+    if (DEBUG)
+        printf("Entering printMap...\n");
+
+    for (const auto& entry : config)
+        //cout << entry.first << ": " << entry.second << "\n";
+        printf("%s: %.2f\n", entry.first.c_str(), entry.second);
+
+    if (DEBUG)
+        printf("Exiting printMap...\n");
+}
+
+
+void action(map<string, double>& config, const char* filename)
+{
+    if (DEBUG)
+        printf("Entering action...\n");
+
+    printMap(config);
+
+    cout << "Would you like to:\n1. Add an item/weight\n2. Edit an item/weight\n3. Calculate "
+         << "final grade\n4. Enter target grade\n\nEnter your choice: ";
+    int choice;
+    cin >> choice;
+
+    switch(choice) {
+        case 1:
+        {
+            string item;
+            cout << "Enter the assignment name: ";
+            cin >> item;
+            double weight;
+            cout << "Enter the assignment's weight: ";
+            cin >> weight;
+
+            addMapItem(config, item, weight, filename);
+            break;
+        }
+        case 2:
+        {
+            printMap(config);
+            string item_edit;
+            cout << "\nWhat item would you like to edit: ";
+            cin >> item_edit;
+
+            break;
+        }
+
+        case 3:
+            break;
+
+        case 4:
+            break;
+
+        // should not be hit if the user can read
+        default:
+            cerr << "Error: invalid operation choice";
+            exit(3);
+    }
+
+    if (DEBUG)
+        printf("Exiting action...\n");
+}
+
+
+map<string, double> addMapItem(map<string, double>& config, const string& item, const double& weight, const char* filename)
+{
+    if (DEBUG)
+        printf("\nEntering addMapItem...\n");
+
+    config.insert(make_pair(item,weight));
+
+    ofstream file (filename);
+    // check file is opened
+    if (!file) {
+        cerr << "Error: file unable to be opened.\n";
+        exit(2);
+    }
+
+    file.close();
+
+    if (DEBUG) {
+        printf("Updated map:\n");
+        printMap(config);
+        printf("\nExiting addMapItem...\n");
     }
 }
 
 
-map<string, double> addMapItem(ifstream&)
+map<string, double> editMapItem(map<string, double>& config, const string& item, const double& weight, const char* filename)
 {
 
 }
+
