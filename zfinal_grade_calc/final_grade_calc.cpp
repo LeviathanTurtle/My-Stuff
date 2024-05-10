96e634114004c892e7afa8b66d4224e3f20f72ed
LeviathanTurtle <wbwads@gmail.com>
2024-05-10 17:24:54 -0400

    added documentation, more progress

--- zfinal_grade_calc/final_grade_calc.cpp
+++ zfinal_grade_calc/final_grade_calc.cpp
@@ -4,10 +4,10 @@
  * 
  * [DESCRIPTION]:
  * This program calculates the final grade in a class. Assignment names and their weighted
- * percentages are stored in an external file named ______. Default assignments are homework,
- * quizzes, tests, and labs. The user has the option to add additional assignments with their
- * weights or change an existing assignments weighted percentage, which will all be saved in the
- * same config file. 
+ * percentages are stored in an external file named config by default, but specified at runtime.
+ * Default assignments are homework, quizzes, tests, and labs. The user has the option to add
+ * additional assignments with their weights or change an existing assignments weighted percentage,
+ * which will all be saved in the same config file. 
  * TODO: maybe a table or some kind of optional structured output
  * 
  * [USAGE]:
@@ -39,20 +39,20 @@ using namespace std;
 bool DEBUG = false;
 
 
-// function to 
+// function to initialize a map from the external file
 map<string, double> initMap(ifstream&);
-// function to 
+// function to print the current map
 void printMap(const map<string, double>&);
-// function to 
+// function that takes input from the user and does what the user specifies
 void action(map<string, double>&, const char*, bool&);
-// function to 
+// function to add an assignment to the current map
 void addMapItem(map<string, double>&, const string&, const double&, const char*);
-// function to 
+// function to edit a name of weight of an assignment
 void editMapItem(map<string, double>&, const string&, const char*);
-// function to 
+// function to calculate the final grade in a class
 double calcFinalGrade(const map<string, double>&);
-// function to 
-double finalExamCalc(const map<string, double>&, const double&);
+// function to calculate the grade you would need on a final to get a certain grade overall
+void finalExamCalc(const map<string, double>&, const double&);
 // function to sort the map items
 void mapSort(map<string, double>&);
 
@@ -71,22 +71,26 @@ int main(int argc, char* argv[])
         // debug
         DEBUG = true;
 
+        // initialize map
         map<string, double> config = initMap(argv[2]);
 
         // prompt user for action
         // bool var for repeating inputs
         bool finished = false;
+        // repeat until the user specified they are finished
         while (!finished)
             action(config, argv[2], finished);
     }
     else {
         // not debug
 
+        // initialize map
         map<string, double> config = initMap(argv[1]);
 
         // prompt user for action
         // bool var for repeating inputs
         bool finished = false;
+        // repeat until the user specified they are finished
         while (!finished)
             action(config, argv[1], finished);
     }
@@ -95,12 +99,18 @@ int main(int argc, char* argv[])
 }
 
 
-// note: this function only initializes the map
+// function to initialize a map from the external file
+/* 
+ * 
+ * 
+ * 
+*/
 map<string, double> initMap(const char* filename)
 {
     if (DEBUG)
         printf("Entering initMap...\n");
 
+    // declare map object
     map<string, double> config;
 
     // defaults
@@ -127,6 +137,7 @@ map<string, double> initMap(const char* filename)
     config[make_tuple(6, "exam")] = 0.25;
     */
 
+    // set up file 
     ifstream file (filename);
     // check file is opened
     if (!file) {
@@ -137,6 +148,7 @@ map<string, double> initMap(const char* filename)
     // load from file
     string category;
     double weight;
+    // while there are file contents, add them to the map
     while (file >> category >> weight)
         //config[category] = weight;
         config.insert(make_pair(category,weight));
@@ -155,6 +167,12 @@ map<string, double> initMap(const char* filename)
 }
 
 
+// function to print the current map
+/* 
+ * 
+ * 
+ * 
+*/
 void printMap(const map<string, double>& config)
 {
     if (DEBUG)
@@ -169,11 +187,18 @@ void printMap(const map<string, double>& config)
 }
 
 
+// function that takes input from the user and does what the user specifies
+/* 
+ * 
+ * 
+ * 
+*/
 void action(map<string, double>& config, const char* filename, bool& finished)
 {
     if (DEBUG)
         printf("Entering action...\n");
 
+    // print the current config for the user
     printMap(config);
 
     cout << "Would you like to:\n1. Add an assignment/weight\n2. Edit an assignment/weight\n3. "
@@ -187,7 +212,7 @@ void action(map<string, double>& config, const char* filename, bool& finished)
         // add item/weight
         {
             string item;
-            cout << "Enter the assignment name: ";
+            cout << "Enter the assignment name. Enter as one word: ";
             cin >> item;
             double weight;
             cout << "Enter the assignment's weight: ";
@@ -214,13 +239,18 @@ void action(map<string, double>& config, const char* filename, bool& finished)
             break;
 
         case 4:
-        // enter target grade -- this is for exam
+        // enter target grade -- this is for final exam
         {
             double target_grade;
             cout << "What final grade would you like to receive: ";
             cin >> target_grade;
+
+            // get final assignment name
+            string final_assignment;
+            cout << "What is the final assignment (e.g. exam, project, etc.): ";
+            cin >> final_assignment;
             
-            printf("You need a %.2f on the exam to get a %.2f\n",finalExamCalc(config,target_grade),target_grade);
+            finalExamCalc(config, target_grade);
             break;
         }
         
@@ -240,21 +270,32 @@ void action(map<string, double>& config, const char* filename, bool& finished)
 }
 
 
+// function to add an assignment to the current map
+/* 
+ * 
+ * 
+ * 
+*/
 void addMapItem(map<string, double>& config, const string& item, const double& weight, const char* filename)
 {
     if (DEBUG)
         printf("\nEntering addMapItem...\n");
 
+    // add the item to the map
     config.insert(make_pair(item,weight));
     // resort the map
     mapSort(config);
 
+    // set up file output
     ofstream file (filename);
     // check file is opened
     if (!file) {
         cerr << "Error: file unable to be opened.\n";
         exit(2);
     }
+
+    // output new assignment to file
+    file << item << " " << weight << endl;
 
     file.close();
 
@@ -266,6 +307,12 @@ void addMapItem(map<string, double>& config, const string& item, const double& w
 }
 
 
+// function to edit a name of weight of an assignment
+/* 
+ * 
+ * 
+ * 
+*/
 void editMapItem(map<string, double>& config, const string& item, const char* filename)
 {
     // the original approach would read from the file until it reached the target assignment,
@@ -285,12 +332,14 @@ void editMapItem(map<string, double>& config, const string& item, const char* fi
     // resort the map
     mapSort(config);
 
+    // remove the file
     string command = "rm " + string(filename);
     int result = system(command.c_str());
     // check result
     if (result == -1)
         cerr << "Error executing command" << endl;
 
+    // set up file output
     ofstream file (filename);
     // check file is opened
     if (!file) {
@@ -298,15 +347,18 @@ void editMapItem(map<string, double>& config, const string& item, const char* fi
         exit(2);
     }
 
+    // for each assignment, output its name and weight
     for (const auto& entry : config)
         file << entry.first << " " << entry.second << "\n";
     
     file.close();
 
-    // check that all weights = 1 (100)
+    // check that all weights = 1 (100%)
     double weight_check = 0;
+    // sum the weights of each assignment
     for (const auto& entry : config)
         weight_check += entry.second;
+    // if the sum of the weights do not equal 1 (100%), output error, re-call function
     if(weight_check != 1) {
         cerr << "Error: weights do not sum to 100\n";
         editMapItem(config, item, filename);
@@ -317,11 +369,19 @@ void editMapItem(map<string, double>& config, const string& item, const char* fi
 }
 
 
+// function to calculate the final grade in a class
+/* 
+ * 
+ * 
+ * 
+*/
 double calcFinalGrade(const map<string, double>& config)
 {
     if (DEBUG)
         printf("Entering calcFinalGrade...\n");
     
+    // create an array of the same size of the map
+    // each index will contain the number of assignments corresponding to the map
     int* num_assignments = new int [config.size()];
     // array control for ^
     int index = 0;
@@ -332,7 +392,9 @@ double calcFinalGrade(const map<string, double>& config)
         cout << "Enter the number of " << entry.first << " assignments: ";
         cin >> num_input;
 
+        // store in array
         num_assignments[index] = num_input;
+        // increment index for next assignment
         index++;
     }
     // check all are accounted for
@@ -353,10 +415,11 @@ double calcFinalGrade(const map<string, double>& config)
             cout << toupper(entry.first[0]) << i+1 << ": ";
             cin >> grade;
 
-            // add sum
+            // add to assignment sum
             grade_sum += grade;
         }
 
+        // add to total sum
         global_sum += (grade_sum/num_assignments[j]) * entry.second;
 
         // increment for next assignment number
@@ -370,15 +433,29 @@ double calcFinalGrade(const map<string, double>& config)
 }
 
 
-double finalExamCalc(const map<string, double>& config, const double& target_grade)
+// function to calculate the grade you would need on a final to get a certain grade overall
+/* 
+ * 
+ * 
+ * 
+*/
+void finalExamCalc(const map<string, double>& config, const double& target_grade)
 {
     if (DEBUG)
         printf("Entering finalExamCalc...\n");
-
-    string final_assignment;
-    cout << "What is the final assignment (e.g. exam, project, etc.): ";
-    cin >> final_assignment;
     
+    // check if it's in the map
+    for (const auto& entry : config) {
+        if (entry.first ==)
+    }
+
+
+
+
+
+    
+    
+    // get final assignment weight 
     double final_weight;
     cout << "What is the weighted percentage of the final (enter XY%): ";
     cin >> final_weight;
@@ -387,17 +464,25 @@ double finalExamCalc(const map<string, double>& config, const double& target_gra
         cout << "Error: final assignment weight cannot exceed 100%. Re-enter: ";
         cin >> final_weight;
     }
+    // I think this is ok?
+    while ()
     
     // define the weight of the current grade
-    double current_grade_weight = 1-final_weight;
+    double current_grade_weight = 1-(final_weight/100);
 
     if (DEBUG)
         printf("Exiting finalExamCalc...\n");
 
-    return (target_grade - calcFinalGrade(config) * current_grade_weight)/final_weight;
+    //return (target_grade - calcFinalGrade(config) * current_grade_weight)/final_weight;
 }
 
 
+// function to sort the map items
+/* 
+ * 
+ * 
+ * 
+*/
 void mapSort(map<string, double>& config)
 {
     if (DEBUG)
