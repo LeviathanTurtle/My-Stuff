b2be05868b9943cbc52be4352ea637760be8338b
LeviathanTurtle <wbwads@gmail.com>
2024-06-13 10:11:29 -0400

    added four function calc

--- libraries/calculator.cpp
+++ libraries/calculator.cpp
@@ -11,6 +11,138 @@
 #include <tuple>
 
 
+/* function to 
+ * pre-condition: 
+ * 
+ * post-condition: 
+*/
+void calculator::set_a(const int& new_a)
+{
+    a = new_a;
+}
+
+
+/* function to 
+ * pre-condition: 
+ * 
+ * post-condition: 
+*/
+int calculator::get_a()
+{
+    return a;
+}
+
+
+/* function to 
+ * pre-condition: 
+ * 
+ * post-condition: 
+*/
+void calculator::set_b(const int& new_b)
+{
+    b = new_b;
+}
+
+
+/* function to 
+ * pre-condition: 
+ * 
+ * post-condition: 
+*/
+int calculator::get_b()
+{
+    return b;
+}
+
+
+/* function to 
+ * pre-condition: 
+ * 
+ * post-condition: 
+*/
+void calculator::set_c(const int& new_c)
+{
+    c = new_c;
+}
+
+
+/* function to 
+ * pre-condition: 
+ * 
+ * post-condition: 
+*/
+int calculator::get_c()
+{
+    return c;
+}
+
+
+/* function to 
+ * pre-condition: 
+ * 
+ * post-condition: 
+*/
+void calculator::set_point_x(const double& new_point_x)
+{
+    point_x = new_point_x;
+}
+
+
+/* function to 
+ * pre-condition: 
+ * 
+ * post-condition: 
+*/
+double calculator::get_point_x()
+{
+    return point_x;
+}
+
+
+/* function to 
+ * pre-condition: 
+ * 
+ * post-condition: 
+*/
+void calculator::set_point_y(const double& new_point_y)
+{
+    point_y = new_point_y;
+}
+
+
+/* function to 
+ * pre-condition: 
+ * 
+ * post-condition: 
+*/
+double calculator::get_point_y()
+{
+    return point_y;
+}
+
+
+/* function to 
+ * pre-condition: 
+ * 
+ * post-condition: 
+*/
+void calculator::set_point_z(const double& new_point_z)
+{
+    point_z = new_point_z;
+}
+
+
+/* function to 
+ * pre-condition: 
+ * 
+ * post-condition: 
+*/
+double calculator::get_point_z()
+{
+    return point_z;
+}
+
+
 /* function to return the minimum number of coins given a monetary value
  * pre-condition: monetary total parameter should be initialized to a non-zero and non-negative
  *                float 
@@ -24,7 +156,7 @@ std::tuple<int, int, int, int> calculator::findCoinTotal(double total)
     // check amount, must be > 0
     if(total <= 0) {
         std::cerr << "error: amount must be greater than 0.\n";
-        return std::make_tuple(-1,-1,-1,-1);
+        throw std::invalid_argument("invalid total");
     }
 
     // convert dollars to cents to avoid floating-point issues
@@ -66,7 +198,7 @@ long int calculator::factorial(const int& endpoint, const bool& double_factorial
     // check that the user's endpoint is valid. Will probably update this later to be more dynamic
     if (endpoint < 0 || endpoint > 1000) {
         std::cerr << "Not valid, endpoint integer must be between 0 and 1,000\n";
-        return -1;
+        throw std::invalid_argument("invalid endpoint");
     }
 
     long int prod = INT_MIN;
@@ -118,12 +250,39 @@ double calculator::geoseries(double a, const int& num_terms, double r=0.5)
 }
 
 
-/* function to 
- * pre-condition: 
+/* function that returns the sum, difference, product, or quotient of two numbers (supports 
+ * integers and floats). 
+ * pre-condition: operand_1 and operand_2 parameters must be initialized with values. If dividing,
+ *                operand_2 cannot be 0. operation parameter must be initialized to a non-empty
+ *                string
  * 
- * post-condition: 
+ * post-condition: depending on the operation specified (assuming the operation is valid), the sum,
+ *                 difference, product, or quotient is returned, otherwise an error is output and a
+ *                 relevant exception is thrown
 */
+template <typename T>
+T calculator::fourFunction(const T& operand_1, const T& operand_2, const std::string& operation)
+{
+    // convert string to lowercase
+    std::transform(operation.begin(), operation.end(), operation.begin(), ::tolower);
 
+    if (operation == "add") {
+        return operand_1 + operand_2;
+    } else if (operation == "subtract") {
+        return operand_1 - operand_2;
+    } else if (operation == "multiply") {
+        return operand_1 * operand_2;
+    } else if (operation == "divide") {
+        if (operand_2 == 0) {
+            std::cerr << "Error: cannot divide by 0\n";
+            throw std::invalid_argument("cannot divide by 0");
+        } else
+            return operand_1 / operand_2;
+    } else {
+        std::cerr << "Error: invalid operation\n";
+        throw std::invalid_argument("invalid operation");
+    }
+}
 
 
 /* function to 
--- libraries/calculator.h
+++ libraries/calculator.h
@@ -13,12 +13,29 @@
 
 class calculator {
     private:
-
+        int a, b, c;
+        int point_x, point_y, point_z;
 
     public:
+        void set_a(const int&);
+        void set_b(const int&);
+        void set_c(const int&);
+        int get_a();
+        int get_b();
+        int get_c();
+        void set_point_x(const double&);
+        void set_point_y(const double&);
+        void set_point_z(const double&);
+        double get_point_x();
+        double get_point_y();
+        double get_point_z();
+
         std::tuple<int, int, int, int> findCoinTotal(double);
         long int factorial(const int&, const bool&);
         double geoseries(double, const int&, double);
+        
+        template <typename T>
+        T calculator::fourFunction(const T&, const T&, const std::string&);
 };
 
 #endif
