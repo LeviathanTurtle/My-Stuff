3effac32b9fa0d31b70c7ae73986d1ea72217ce9
LeviathanTurtle <wbwads@gmail.com>
2024-06-14 12:15:42 -0400

    added a few geometric shape funcs

--- libraries/calculator.cpp
+++ libraries/calculator.cpp
@@ -9,134 +9,75 @@
 #include "calculator.h"
 #include <iostream>
 #include <tuple>
+#include <numbers>       // std::numbers::pi
 
 
-/* function to 
- * pre-condition: 
- * 
- * post-condition: 
-*/
-void calculator::set_a(const int& new_a)
+// function to update the value of a
+void calculator::set_a(const double& new_a)
 {
     a = new_a;
 }
-
-
-/* function to 
- * pre-condition: 
- * 
- * post-condition: 
-*/
-int calculator::get_a()
+// function to return the current a value
+double calculator::get_a()
 {
     return a;
 }
 
 
-/* function to 
- * pre-condition: 
- * 
- * post-condition: 
-*/
-void calculator::set_b(const int& new_b)
+// function to update the value of b
+void calculator::set_b(const double& new_b)
 {
     b = new_b;
 }
-
-
-/* function to 
- * pre-condition: 
- * 
- * post-condition: 
-*/
-int calculator::get_b()
+// function to return the current b value
+double calculator::get_b()
 {
     return b;
 }
 
 
-/* function to 
- * pre-condition: 
- * 
- * post-condition: 
-*/
-void calculator::set_c(const int& new_c)
+// function to update the value of c
+void calculator::set_c(const double& new_c)
 {
     c = new_c;
 }
-
-
-/* function to 
- * pre-condition: 
- * 
- * post-condition: 
-*/
-int calculator::get_c()
+// function to return the current c value
+double calculator::get_c()
 {
     return c;
 }
 
 
-/* function to 
- * pre-condition: 
- * 
- * post-condition: 
-*/
+// function to update the value of point_x
 void calculator::set_point_x(const double& new_point_x)
 {
     point_x = new_point_x;
 }
-
-
-/* function to 
- * pre-condition: 
- * 
- * post-condition: 
-*/
+// function to return the current point_x value
 double calculator::get_point_x()
 {
     return point_x;
 }
 
 
-/* function to 
- * pre-condition: 
- * 
- * post-condition: 
-*/
+// function to update the value of point_y
 void calculator::set_point_y(const double& new_point_y)
 {
     point_y = new_point_y;
 }
-
-
-/* function to 
- * pre-condition: 
- * 
- * post-condition: 
-*/
+// function to return the current point_y value
 double calculator::get_point_y()
 {
     return point_y;
 }
 
 
-/* function to 
- * pre-condition: 
- * 
- * post-condition: 
-*/
+// function to update the value of point_z
 void calculator::set_point_z(const double& new_point_z)
 {
     point_z = new_point_z;
 }
-
-
-/* function to 
- * pre-condition: 
- * 
- * post-condition: 
-*/
+// function to return the current point_z value
 double calculator::get_point_z()
 {
     return point_z;
@@ -228,9 +169,11 @@ long int calculator::factorial(const int& endpoint, const bool& double_factorial
 
 
 /* function to return the geometric series of a number
- * pre-condition: 
+ * pre-condition: starting value a must be initialized to a non-zero float, number of terms must be
+ *                initialized to a non-zero integer, r must be initialized to a positive non-zero
+ *                float, but has a default of 0.5 if it is not provided or invalid
  * 
- * post-condition: 
+ * post-condition: the series sum is returned
 */
 double calculator::geoseries(double a, const int& num_terms, double r=0.5)
 {
@@ -266,22 +209,70 @@ T calculator::fourFunction(const T& operand_1, const T& operand_2, const std::st
     // convert string to lowercase
     std::transform(operation.begin(), operation.end(), operation.begin(), ::tolower);
 
-    if (operation == "add") {
+    if (operation == "add")
         return operand_1 + operand_2;
-    } else if (operation == "subtract") {
+    else if (operation == "subtract")
         return operand_1 - operand_2;
-    } else if (operation == "multiply") {
+    else if (operation == "multiply")
         return operand_1 * operand_2;
-    } else if (operation == "divide") {
+    else if (operation == "divide")
         if (operand_2 == 0) {
             std::cerr << "Error: cannot divide by 0\n";
             throw std::invalid_argument("cannot divide by 0");
         } else
             return operand_1 / operand_2;
-    } else {
+    else {
         std::cerr << "Error: invalid operation\n";
         throw std::invalid_argument("invalid operation");
     }
+}
+
+
+/* function to calculate the distance between two cartesian coordinates (x1,y1,x2,y2)
+ * pre-condition: the coordinates (passed as (x1,y1,x2,y2)) should be initialized as valid
+ *                cartesian coordinate points
+ * 
+ * post-condition: the distance between the two points is returned
+*/
+// todo: double check this
+double calculator::distance(const double& q, const double& w, const double& e, const double& r)
+{
+    return sqrt(pow(e-q,2) + pow(r-w,2));
+}
+
+
+/* function to calculate the radius between two cartesian coordinates (x1,y1,x2,y2)
+ * pre-condition: the coordinates (passed as (x1,y1,x2,y2)) should be initialized as valid
+ *                cartesian coordinate points 
+ * 
+ * post-condition: the radius between the two points is returned
+*/
+// todo: double check this
+double calculator::radius(const double& a, const double& s, const double& d, const double& f)
+{
+    return distance(a,s,d,f);
+}
+
+
+/* function to determine the circumference of a circle
+ * pre-condition: the radius must be initialized to a positive non-zero float
+ * 
+ * post-condition: the calculated circumference is returned
+*/
+double calculator::circumference(const double& radius)
+{
+    return 2*std::numbers::pi * radius;
+}
+
+
+/* function to calculate the area of a circle
+ * pre-condition: the radius must be initialized to a positive non-zero float
+ * 
+ * post-condition: the calculated area is returned
+*/
+double calculator::area_circle(const double& radius)
+{
+    return std::numbers::pi * pow(radius, 2);
 }
 
 
@@ -293,8 +284,5 @@ T calculator::fourFunction(const T& operand_1, const T& operand_2, const std::st
 
 
 
-/* function to 
- * pre-condition: 
- * 
- * post-condition: 
-*/
+
+
--- libraries/calculator.h
+++ libraries/calculator.h
@@ -13,16 +13,16 @@
 
 class calculator {
     private:
-        int a, b, c;
-        int point_x, point_y, point_z;
+        double a, b, c;
+        double point_x, point_y, point_z;
 
     public:
-        void set_a(const int&);
-        void set_b(const int&);
-        void set_c(const int&);
-        int get_a();
-        int get_b();
-        int get_c();
+        void set_a(const double&);
+        void set_b(const double&);
+        void set_c(const double&);
+        double get_a();
+        double get_b();
+        double get_c();
         void set_point_x(const double&);
         void set_point_y(const double&);
         void set_point_z(const double&);
@@ -35,7 +35,12 @@ class calculator {
         double geoseries(double, const int&, double);
         
         template <typename T>
-        T calculator::fourFunction(const T&, const T&, const std::string&);
+        T fourFunction(const T&, const T&, const std::string&);
+
+        double distance(const double&, const double&, const double&, const double&);
+        double radius(const double&, const double&, const double&, const double&);
+        double circumference(const double&);
+        double area_circle(const double&);
 };
 
 #endif
