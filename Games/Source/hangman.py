1a47f6a4b5817f719c14ec2c07601bf8710d438d
LeviathanTurtle <wbwads@gmail.com>
2024-08-16 20:45:50 -0400

    began pep8-ing python stuff

--- Games/Source/hangman.py
+++ Games/Source/hangman.py
@@ -4,6 +4,7 @@
 # Created: 11.3.2020
 # Doctored: 10.16.2023
 # Python-ized: 3.30.2024
+# Updated 8.16.2024: function decomposition and PEP 8 Compliance
 # 
 # [DESCRIPTION]:
 # This program is hangman the game.
@@ -24,12 +25,15 @@ using namespace std;
 
 #define MAX_MISTAKES 100
 """
-import sys
-import random
-import time
+from sys import argv, stderr
+from random import seed, randint
+from time import time
+from typing import List
 
 # max amount of mistakes user can make
-MAX_MISTAKES = 10
+MAX_MISTAKES: int = 10
+
+DEBUG: bool = False
 
 # --- FUNCTIONS -------------------------------------------------------------------------
 # --- USER GUESS --------------------------------
@@ -60,183 +64,175 @@ void userGuess(string word, string& guessWord, string& used, int& incorrect)
     }
 }
 """
-def userGuess(word, guess_word, used, incorrect) -> int:
-    guess = input("\nGuess a letter: ")
+def user_guess(word: str, guess_word: str, used_letters: str, incorrect_guesses: int) -> int:
+    """Handle the user's letter guess and update the game state."""
+    
+    if DEBUG:
+        print("Entering user_guess...")
+    
+    guess = input("\nGuess a letter: ").lower()
     
     # check if the user has used this letter
-    #while checkMatch(guess,used):
-    while guess in used:
+    while guess in used_letters:
         # if user guesses letter again
-        print("You already guessed ", guess)
-        guess = input("Guess another letter: ")
+        print(f"You already guessed '{guess}'")
+        guess = input("Guess another letter: ").lower()
 
     # if the letter is in the word, update guessWord
-    #if checkMatch(guess,word):
     if guess in word:
         # update guessWord with correct guessed letters
-        print(guess,"is in the word")
+        print(f"'{guess}' is in the word")
         
-        for i in range(len(word)):
-            if(word[i] == guess):
+        for i, letter in enumerate(word):
+            if letter == guess:
                 guess_word[i] = guess
         # update used letters
-        used.append(guess)
+        used_letters.append(guess)
             
     # letter is not in word
     else:
         # add to mistake counter if incorrect
-        print(guess,"is not in the word")
+        print(f"'{guess}' is not in the word")
         # update used words string/array
         #used = used + guess + " "
-        used.append(guess)
+        used_letters.append(guess)
         # increment incorrect number of guesses
-        incorrect += 1
+        incorrect_guesses += 1
     
-    return incorrect
+    if DEBUG:
+        print("Exiting user_guess.")
+    return incorrect_guesses
 
-# --- CHECK MATCH -------------------------------
-"""
-bool checkMatch(char, string);
-bool checkMatch(char letter, string word)
-{
-    return (word.find(letter) != string::npos);
-}
-"""
-def checkMatch(letter, word) -> bool:
-    return letter in word
 
-# --- MAIN ------------------------------------------------------------------------------
-# --- SETUP -------------------------------------
-"""
-int main(int argc, char* argv[])
-{
-    srand(static_cast<unsigned>(time(nullptr)));
-    
-    ifstream inputFile;
-    inputFile.open(argv[1]);
-
-    if(!inputFile) {
-        cerr << "error: file name provided (" << argv[1] << ") is invalid or not found. quitting...\n";
-        exit(1);
-    }
-"""
-# seed the random number generator -- ChatGPT
-random.seed(time.time())
-
-# get filename from argv
-with open(sys.argv[1],'r') as input_file:
-    # check if file name provided is valid. if not, throw error and quit
-    if not input_file:
-        sys.stderr.write(f"error: file name provided ({sys.argv[1]}) is invalid or not found.")
-        exit(1)
-    
-# --- READ FILE ---------------------------------
+def main():
+    # --- SETUP ---------------------------------
     """
-    int wordBankNum;
-    inputFile >> wordBankNum;
-
-    string word;
-    for(int i=0; i<rand()%wordBankNum; i++)
-        inputFile >> word;
-
-    inputFile.close();
-    """
-    # get number of words (should be first item in file)
-    word_bank_num = int(input_file.readline())
-    
-    # get random word from file
-    for _ in range(random.randint(0, word_bank_num-1)):
-        word = input_file.readline().strip()
-
-# --- SET UP VARS -------------------------------
-"""
-    int incorrect = 0;
-
-    string used = "";
-
-    string evol = "";
-    
-    for(size_t i=0; i<word.length(); i++)
-        evol += "_";
-"""
-# mistakes user has made
-#incorrect = 0
-incorrect = 0
-# string of used letters, starts empty
-#used = ""
-used = []
-# word with guessed letters, starts empty
-#evol = ""
-# initialize to blanks
-#for _ in range(len(word)):
-#    evol += "_"
-evol = ["_"] * len(word)
-
-# --- TITLE -------------------------------------
-"""
-    cout << setw(15) << "Hangman" << endl << endl;
-"""
-print(f"{'Hangman':>15}\n")
-
-# --- MAIN LOOP -------------------------------------------------------------------------
-# --- PRINT BLANKS ------------------------------
-"""
-    while (/*finish == false &&*/ incorrect < MAX_MISTAKES)
+    int main(int argc, char* argv[])
     {
-        for (size_t i=0; i<word.length(); i++)
-            cout << evol[i] << " ";
-
-        cout << endl << endl;
-"""
-# user must not be finished and have no more than 10 mistakes
-while incorrect < MAX_MISTAKES:
-    # print blanks for word
-    for i in range(len(word)):
-        print(f"{evol[i]} ",end="")
-    # extra space
-    print()
-
-# --- SHOW REMAINING MISTAKES -------------------
-    """
-        cout << "Remaining guesses: " << (MAX_MISTAKES - incorrect) << endl;
+        srand(static_cast<unsigned>(time(nullptr)));
         
-        cout << "Used letters: " << used << endl;
-    """
-    # show user remaining mistakes
-    print("Remaining guesses:",MAX_MISTAKES-incorrect)
-    # show user guessed words
-    print("Used letters:",used)
+        ifstream inputFile;
+        inputFile.open(argv[1]);
 
-# --- ADD GUESSED LETTERS -----------------------
-    """
-        userGuess(word,evol,used,incorrect);
-    """
-    # add guessed letters to string
-    incorrect = userGuess(word,evol,used,incorrect)
-
-# --- CHECK MISTAKES ----------------------------
-    """
-    if(incorrect < MAX_MISTAKES && evol.compare(word)==0) {
-            cout << "\nCongratulations, you win! The word was " << word << endl;
-            return 0;
+        if(!inputFile) {
+            cerr << "error: file name provided (" << argv[1] << ") is invalid or not found. quitting...\n";
+            exit(1);
         }
-    NOTE: this was moved here to keep things consistent
+        
+        int wordBankNum;
+        inputFile >> wordBankNum;
+
+        string word;
+        for(int i=0; i<rand()%wordBankNum; i++)
+            inputFile >> word;
+
+        inputFile.close();
+    """
+    # seed the random number generator
+    seed(time())
     
-    if (incorrect == MAX_MISTAKES) {
-            cout << "\nYou lose. The word was " << word << endl;
-            return 0;
-        }
-    }
+    if len(argv) != 2:
+        stderr.write("Usage: python hangman.py <wordlist_file>\n")
+        exit(1)
 
-    return 0;
-}
+    # get filename from argv
+    try:
+        with open(argv[1], 'r') as input_file:
+            word_bank_num = int(input_file.readline().strip())
+
+            for _ in range(randint(0, word_bank_num - 1)):
+                word = input_file.readline().strip()
+    except (FileNotFoundError, ValueError) as e:
+        stderr.write(f"Error: {str(e)}\n")
+        exit(1)
+
+    # --- SET UP VARS ---------------------------
     """
-    evol_str = ''.join(evol)
-    # if the user correctly guessed the word, output to reflect
-    if incorrect < MAX_MISTAKES and evol_str == word:
-        print("\nCongratulations, you win! The word was",word)
-        exit(0)
-    # if user makes 10 mistakes, show word and quit program
-    elif incorrect == MAX_MISTAKES:
-        print("\nYou lose. The word was",word)
-        exit(0)
+        int incorrect = 0;
+
+        string used = "";
+
+        string evol = "";
+        
+        for(size_t i=0; i<word.length(); i++)
+            evol += "_";
+    """
+    # mistakes user has made
+    incorrect_guesses: int = 0
+    # string of used letters, starts empty
+    used_letters: List[str] = []
+    # word with guessed letters, initialize to blanks
+    guess_word: str = ["_"] * len(word)
+
+    # --- TITLE ---------------------------------
+    """
+        cout << setw(15) << "Hangman" << endl << endl;
+    """
+    print(f"{'Hangman':>15}\n")
+
+    # --- MAIN LOOP ---------------------------------------------------------------------
+    # --- PRINT BLANKS --------------------------
+    """
+        while (/*finish == false &&*/ incorrect < MAX_MISTAKES)
+        {
+            for (size_t i=0; i<word.length(); i++)
+                cout << evol[i] << " ";
+
+            cout << endl << endl;
+    """
+    # user must not be finished and have no more than 10 mistakes
+    while incorrect_guesses < MAX_MISTAKES:
+        # print blanks for word
+        """for i in range(len(word)):
+            print(f"{evol[i]} ",end="")
+        # extra space
+        print()"""
+        print(' '.join(guess_word))
+
+    # --- SHOW REMAINING MISTAKES ---------------
+        """
+            cout << "Remaining guesses: " << (MAX_MISTAKES - incorrect) << endl;
+            
+            cout << "Used letters: " << used << endl;
+        """
+        # show user remaining mistakes
+        print(f"\nRemaining guesses: {MAX_MISTAKES-incorrect_guesses}")
+        # show user guessed words
+        print(f"Used letters: {', '.join(used_letters)}")
+
+    # --- ADD GUESSED LETTERS -------------------
+        """
+            userGuess(word,evol,used,incorrect);
+        """
+        # add guessed letters to string
+        incorrect_guesses = user_guess(word,guess_word,used_letters,incorrect_guesses)
+
+    # --- CHECK MISTAKES ----------------------------
+        """
+        if(incorrect < MAX_MISTAKES && evol.compare(word)==0) {
+                cout << "\nCongratulations, you win! The word was " << word << endl;
+                return 0;
+            }
+        NOTE: this was moved here to keep things consistent
+        
+        if (incorrect == MAX_MISTAKES) {
+                cout << "\nYou lose. The word was " << word << endl;
+                return 0;
+            }
+        }
+
+        return 0;
+    }
+        """
+        # if the user correctly guessed the word, output to reflect
+        if ''.join(guess_word) == word:
+            print(f"\nCongratulations, you win! The word was '{word}'")
+            exit(0)
+        # if user makes 10 mistakes, show word and quit program
+        elif incorrect_guesses == MAX_MISTAKES:
+            print(f"\nYou lose. The word was '{word}'")
+            exit(0)
+
+
+if __name__ == "__main__":
+    main()
--- Games/Source/trivia.py
+++ Games/Source/trivia.py
@@ -1,5 +1,7 @@
 # This uses the Trivia API from Open Trivia Database (documentation: https://opentdb.com/api_config.php)
 # 
+# Updated 8.16.2024: function decomposition and PEP 8 Compliance
+# 
 # The base link is: https://opentdb.com/api.php
 # where
 #     -- REQUIRED --
@@ -26,52 +28,87 @@
 
 from requests import get
 from html import unescape
+from typing import List, Optional, Tuple, Dict
 
-# ask the user for the amount of jokes they'd like to generate
-num_questions = input("How many trivia questions would you like: ")
+DEBUG: bool = False
 
-# query the API
-response = get(f'https://opentdb.com/api.php?amount={num_questions}')
 
-# successful response code
-if response.status_code == 200:
-    # store response in var
-    data = response.json()
+# pre-condition: num_questions is a positive integer
+# post-condition: returns a list of question data if the API call is successful, otherwise None
+def fetch_trivia_questions(num_questions: int) -> Optional[List[Dict[str, str]]]:
+    """Fetch trivia questions from the OpenTrivia Database API"""
     
-    # for each question in the response
-    for i, result in enumerate(data['results']):
-        # extract the question and answers from the response
-        question = result['question']
-        correct_answer = result['correct_answer']
-        incorrect_answers = result['incorrect_answers']
-        
-        # convert any HTML entities to their characters
-        question = unescape(question)
-        correct_answer = unescape(correct_answer)
-        incorrect_answers = [unescape(ans) for ans in incorrect_answers]
-        
-        print(f"\n  --- Trivia Question {i+1} ---\n{question}")
-        
-        # display answer choices
-        all_answers = incorrect_answers + [correct_answer]
-        #print("\nAnswer choices:")
-        for j, answer in enumerate(all_answers, 1):
-            print(f"{j}. {answer}")
-        
-        user_answer = input("\n: ")
-        
-        # checking user's answer
-        while not user_answer.isdigit():
-            user_answer = input("\nPlease enter a number: ")
+    if DEBUG:
+        print("Entering fetchTriviaQuestions...")
+    
+    # query the API
+    response = get(f'https://opentdb.com/api.php?amount={num_questions}')
+    
+    if response.status_code == 200:
+        if DEBUG:
+            print("Exiting fetchTriviaQuestions.")
+        return response.json()['results']
+    # query unsuccessful
+    else:
+        # output error code
+        print(f"Error: {response.status_code}\nFailed to retrieve trivia question(s)")
+        if DEBUG:
+            print("Exiting fetchTriviaQuestions.")
+        return None
+
+
+# pre-condition: question_data is a dictionary containing the question, correct answer, and
+#                incorrect answers, index is a non-negative integer representing the question
+#                number
+# post-condition: returns a tuple containing the user's answer (as an integer), the correct answer
+#                 (as a string), and a list of all possible answers (as strings)
+def display_question(question_data: Dict[str, str], index: int) -> Tuple[int, str, List[str]]:
+    """Display a trivia question and return user's answer."""
+    
+    if DEBUG:
+        print("Entering displayQuestion...")
+    
+    question = unescape(question_data['question'])
+    correct_answer = unescape(question_data['correct_answer'])
+    incorrect_answers = [unescape(ans) for ans in question_data['incorrect_answers']]
+    
+    print(f"\n  --- Trivia Question {index+1} ---\n{question}")
+    
+    # display answer choices
+    all_answers = incorrect_answers + [correct_answer]
+    for i, answer in enumerate(all_answers, 1):
+        print(f"{i}. {answer}")
+    
+    user_answer = input("\n: ")
+    
+    # checking user's answer
+    while not user_answer.isdigit() or not (1 <= int(user_answer) <= len(all_answers)):
+        user_answer = input(f"Please enter a number between 1 and {len(all_answers)}: ")
+    
+    if DEBUG:
+        print("Exiting displayQuestion.")
+    return int(user_answer), correct_answer, all_answers
+
+
+def main() -> None:
+    # ask the user for the amount of jokes they'd like to generate
+    num_questions = input("How many trivia questions would you like: ")
+    
+    if not num_questions.isdigit() or int(num_questions) <= 0:
+        print("Please enter a valid positive integer.")
+        return
+
+    questions = fetch_trivia_questions(num_questions)
+
+    if questions:
+        for i, question_data in enumerate(questions):
+            user_answer, correct_answer, all_answers = display_question(question_data, i)
             
-        if 1 <= int(user_answer) <= len(all_answers):
-            if all_answers[int(user_answer) - 1] == correct_answer:
+            if all_answers[user_answer - 1] == correct_answer:
                 print("Correct!")
             else:
                 print(f"Wrong. The correct answer was: {correct_answer}")
-        else:
-            print(f"Please enter a number between 1 and {len(all_answers)}.")
-# query unsuccessful
-else:
-    # output error code
-    print(f"Error: {response.status_code}\nFailed to retrieve trivia question(s)")
+
+
+if __name__ == "__main__":
+    main()
