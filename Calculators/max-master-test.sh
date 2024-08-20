d4a1d7fb96b18de88a0aa07b81287b84efbf1005
LeviathanTurtle <wbwads@gmail.com>
2024-08-19 21:39:36 -0400

    removed scripts (handled by CMake)

--- Calculators/max-master-test.sh
+++ Calculators/max-master-test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# This script assumes max-master.cpp is compiled as "max-master-cpp" and is in the same directory
+
 for i in {1..20..1}
 do
-    ./max-master-cpp.exe 1000 10
+    ./max-master-cpp 1000 10
 done
