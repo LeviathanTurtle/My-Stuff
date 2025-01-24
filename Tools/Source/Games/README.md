890b04775ce047f4e0bf454a30e99861604b0435
LeviathanTurtle <wbwads@gmail.com>
2025-01-23 23:15:19 -0500

    macro updates

--- Tools/Source/Games/README.md
+++ Tools/Source/Games/README.md
@@ -8,7 +8,6 @@ Well, a potential problem with this is in the highest difficulty (as of Patch 7)
 
 One of the things that makes Sorcerers unique is that they have access to what's called Sorcery Points (also refreshed after a long rest), and they can be used to activate certain class [Metamagic](https://bg3.wiki/wiki/Metamagic) or create spell slots.
 
-This macro utilizes the infinite sorcery points and spell slots [exploit](https://youtu.be/McnZwKkqanQ?si=U0h2VT1BLgwUji96) (Note: the video title is clickbait, it is SFW). This macro utilizes the second method outlined in the video.
+This macro utilizes the infinite sorcery points and spell slots [exploit](https://youtu.be/McnZwKkqanQ?si=U0h2VT1BLgwUji96) (Note: the video title is clickbait; it is SFW). This macro utilizes the second or third methods outlined in the video. I opted to use these two because they are not as expensive as repeatedly purchasing Angelic Reprieve potions. Method 2 also only requires a one-time purchase, and is available earlier than the third and fourth methods, while also adhering to the choices I would most likely make in a playthrough.
 
-, since using the Angelic potions can be expensive
-
+Note that the mouse positions in the macro are the measurements I made, so they may not be the same for you. I recommend testing the mouse positions first with `mouse_pos.py` before running the macro. Also be sure to adjust the spell slot and sorcerer point amounts to your liking before using.
--- Tools/Source/Games/bg3_inf_sorcspell.py
+++ Tools/Source/Games/bg3_inf_sorcspell.py
@@ -1,5 +1,12 @@
+
+# 
+# William Wadsworth
+# 1.23.2025
+# 
+
 import keyboard
-import pyautogui
+#import pyautogui
+import pydirectinput
 import time
 
 # INFORMATION:
@@ -20,32 +27,37 @@ Base_spellslots_2 = 3
 Base_spellslots_3 = 3
 Base_spellslots_4 = 3
 Base_spellslots_5 = 1
+using_shield_of_devotion = True
+using_spell_savant_amulet = False
+#MOUSE_SPEED = 1
+SLEEP_DURATION = .4
 
 # LAYOUT: 
 # TARGET GOAL -> need:
 # I: 20   -> +16 slots (32 pts)
 Target_spellslots_1 = 20
 Spell_1_pts = 32
+
 # II: 20  -> +17 slots (51 pts)
 unlocked_spellslots_2 = True
 Target_spellslots_2 = 20
 Spell_2_pts = 51
-# III: 10 -> +7 slots (35 pts)
-unlocked_spellslots_3 = True
-Target_spellslots_3 = 10
-Spell_3_pts = 35
-# IV: 10  -> +7 slots (42 pts)
-unlocked_spellslots_4 = True
-Target_spellslots_4 = 10
-Spell_4_pts = 42
-# V: 5    -> +4 slots (28 pts)
-unlocked_spellslots_5 = True
-Target_spellslots_5 = 5
-Spell_5_pts = 28
-
 sorcery_pts = 30
 
-#total: 188 pts needed + 30 pts for metamagic
+# III: 10 -> +7 slots (35 pts)
+unlocked_spellslots_3 = False
+Target_spellslots_3 = 10
+Spell_3_pts = 35
+
+# IV: 10  -> +7 slots (42 pts)
+unlocked_spellslots_4 = False
+Target_spellslots_4 = 10
+Spell_4_pts = 42
+
+# V: 5    -> +4 slots (28 pts)
+unlocked_spellslots_5 = False
+Target_spellslots_5 = 5
+Spell_5_pts = 28
 
 # the loop counter needs to be the total amount of sorcery points needed
 LOOP_COUNTER: int = Spell_1_pts
@@ -60,17 +72,17 @@ if unlocked_spellslots_2:
                 LOOP_COUNTER += Spell_5_pts
     
 
-print(LOOP_COUNTER)
+#print(LOOP_COUNTER)
 
 
 """
 def open_notepad_macro():
-    pyautogui.press('win')
+    pydirectinput.press('win')
     time.sleep(1)
-    pyautogui.typewrite('notepad', interval=0.1)
-    pyautogui.press('enter')
+    pydirectinput.typewrite('notepad', interval=0.1)
+    pydirectinput.press('enter')
     time.sleep(1)
-    pyautogui.typewrite('Hello, this is a macro!', interval=0.1)
+    pydirectinput.typewrite('Hello, this is a macro!', interval=0.1)
 
 # Bind the macro to a hotkey (e.g., CTRL+ALT+M)
 keyboard.add_hotkey('ctrl+alt+m', open_notepad_macro)
@@ -78,3 +90,143 @@ keyboard.add_hotkey('ctrl+alt+m', open_notepad_macro)
 # Keep the script running to listen for the hotkey
 keyboard.wait('esc')  # Exit the script by pressing ESC
 """
+
+
+# -------------------------------------------------------------------------------------------------
+
+def mouse_click():
+    pydirectinput.mouseDown()
+    time.sleep(0.1)  # Hold for 100ms
+    pydirectinput.mouseUp()
+
+def select_metamagic(type: str):
+    """function def."""
+    
+    if type == "SORCERY PTS": # 1745 1215
+        pydirectinput.moveTo(1745, 1215)
+        time.sleep(SLEEP_DURATION/2)
+        mouse_click()
+        time.sleep(SLEEP_DURATION)
+    elif type == "SPELL SLOTS": # 1745 1275
+        pydirectinput.moveTo(1745, 1275)
+        time.sleep(SLEEP_DURATION/2)
+        mouse_click()
+        time.sleep(SLEEP_DURATION)
+    else:
+        pass # error
+
+def create_sorcery_pts(spellslot_level: int):
+    """function def."""
+    
+    print(f"Creating {spellslot_level} sorcery pt(s)...")
+    
+    spellslot_level_y = 1250
+    match (spellslot_level):
+        case 1:
+            # 1190 1250
+            spellslot_level_x = 1190
+        case 2:
+            # 1250 1250
+            spellslot_level_x = 1250
+        case 3:
+            # 1300 1250 
+            spellslot_level_x = 1300
+        case 4:
+            # 1365 1250
+            spellslot_level_x = 1365
+        case 5:
+            # 1425 1250
+            spellslot_level_x = 1425
+
+    select_metamagic("SORCERY PTS")
+
+    # select spell slot level
+    pydirectinput.moveTo(spellslot_level_x, spellslot_level_y)
+    time.sleep(SLEEP_DURATION/2)
+    mouse_click()
+    time.sleep(SLEEP_DURATION)
+    
+    # cast mouse pos: 1200 1050
+    pydirectinput.moveTo(1200, 950)
+    time.sleep(SLEEP_DURATION/2)
+    mouse_click()
+    #time.sleep(SLEEP_DURATION)
+    
+    print(f"Created {spellslot_level} sorcery pts")
+
+def create_spellslot(spellslot_level: int):
+    pass
+
+def activate_shield():
+    """function def."""
+    
+    # EVENT LOOP (shield):
+    # 
+    # move mouse (1285 1330)
+    # click (equip shield)
+    # create_sorc_pts
+    # move mouse
+    # click (unequip shield)
+    
+    print("Equipping shield")
+    pydirectinput.moveTo(1285, 1330)
+    time.sleep(SLEEP_DURATION/2)
+    mouse_click()
+    time.sleep(SLEEP_DURATION)
+    
+    # sorc pts
+    create_sorcery_pts(1)
+    time.sleep(2.4)
+    
+    print("Unequipping shield")
+    pydirectinput.moveTo(1285, 1330)
+    time.sleep(SLEEP_DURATION/2)
+    mouse_click()
+    time.sleep(SLEEP_DURATION)
+
+def macro():
+    """function def."""
+    
+    time.sleep(5)
+    #create_sorcery_pts(1)
+    
+    # -----------------
+    for _ in range(Base_spellslots_1):
+        create_sorcery_pts(1)
+        time.sleep(2.4)
+    
+    # get all necessary sorcery pts
+    for _ in range(LOOP_COUNTER):
+        activate_shield()
+        time.sleep(.75)
+    
+    # expand spell slots
+
+
+def main():
+    #time.sleep(5)
+    # Bind the macro to a hotkey (e.g., CTRL+ALT+M)
+    keyboard.add_hotkey('shift+r', macro)
+    
+    # Keep the script running to listen for the hotkey
+    keyboard.wait('esc')  # Exit the script by pressing ESC
+    keyboard.add_hotkey('esc', keyboard.wait)
+
+
+if __name__ == "__main__":
+    main()
+
+
+# EVENT LOOP (amulet):
+# 
+# cast all lvl 2
+# loop:
+#   move mouse
+#   click (equip amulet)
+#   move mouse
+#   click (activate metamagic)
+#   move mouse
+#   click (select level)
+#   move mouse
+#   click (cast)
+
