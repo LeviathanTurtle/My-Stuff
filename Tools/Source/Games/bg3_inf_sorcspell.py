b987cc72e1129a68f0bb4f451f9e63054cd69b7b
LeviathanTurtle <wbwads@gmail.com>
2025-01-25 18:20:59 -0500

    needs testing with amulet

--- Tools/Source/Games/bg3_inf_sorcspell.py
+++ Tools/Source/Games/bg3_inf_sorcspell.py
@@ -7,7 +7,7 @@
 # [notes]
 # 
 
-import keyboard
+from keyboard import add_hotkey, wait
 from pydirectinput import moveTo, mouseDown, mouseUp
 from time import sleep, time
 
@@ -16,6 +16,115 @@ from time import sleep, time
 
 # the length of time to sleep between inputs (in seconds)
 SLEEP_DURATION = .05
+
+def macro():
+    """function def."""
+    
+    #############################################
+    # CHANGE ALL CURRENT VALUES HERE
+    
+    using_shield = True           # set to false if using the amulet
+    unlocked_spellslots_3 = True # set any of these to true if they are unlocked and you want 
+    unlocked_spellslots_4 = True # them expanded
+    unlocked_spellslots_5 = True
+    
+    # UPDATE THESE VALUES TO REFLECT YOUR CURRENT SPELL SLOTS
+    # Note that this number should be considered after all sorc pts are gathered (since we are 
+    # using first and second level spell slots) 
+    
+    if using_shield: # if we use the shield, we will use all first-level spell slots
+        Current_spellslots_1 = 0 # leave this 0
+        Current_spellslots_2 = 3 # default of 3
+    else: # likewise for the amulet, we will use all second-level spell slots
+        Current_spellslots_1 = 4 # default of 4
+        Current_spellslots_2 = 0 # leave this 0
+    Current_spellslots_3 = 3
+    Current_spellslots_4 = 3
+    Current_spellslots_5 = 1
+    Current_sorc_pts = 6
+    
+    # THESE ARE THE TARGET VALUES YOU WANT
+    Target_spellslots_1 = 20 # e.g. I want 20 first-level spell slots
+    Target_spellslots_2 = 20
+    Target_spellslots_3 = 10
+    Target_spellslots_4 = 10
+    Target_spellslots_5 = 5
+    Target_sorc_pts = 30
+    #############################################
+
+    # SETUP: 
+    # TARGET GOAL -> need:
+    # I: 20   -> +16 slots (32 pts) [x2]
+    Target_spellslots_1 -= Current_spellslots_1
+    Spell_1_pts = Target_spellslots_1*2
+    # Note: if we're using the shield, all of these will be depleted so we need to create all of them
+
+    # II: 20  -> +17 slots (51 pts) [x3]
+    Target_spellslots_2 -= Current_spellslots_2
+    Spell_2_pts = Target_spellslots_2*3
+    # Note: if we're using the amulet, all of these will be depleted so we need to create all of them
+    Target_sorc_pts -= Current_sorc_pts 
+
+    # the loop counter needs to be the total amount of sorcery points needed
+    LOOP_COUNTER: int = Spell_1_pts+Spell_2_pts + Target_sorc_pts
+    # III: 10 -> +7 slots (35 pts) [x5]
+    if unlocked_spellslots_3:
+        Target_spellslots_3 -= Current_spellslots_3
+        Spell_3_pts = Target_spellslots_3*5
+        LOOP_COUNTER += Spell_3_pts
+        
+        # IV: 10  -> +7 slots (42 pts) [x6]
+        if unlocked_spellslots_4:
+            Target_spellslots_4 -= Current_spellslots_4
+            Spell_4_pts = Target_spellslots_4*6
+            LOOP_COUNTER += Spell_4_pts
+            
+            # V: 5    -> +4 slots (28 pts) [x7]
+            if unlocked_spellslots_5:
+                Target_spellslots_5 -= Current_spellslots_5
+                Spell_5_pts = Target_spellslots_5*7
+                LOOP_COUNTER += Spell_5_pts
+
+    if not using_shield:
+        LOOP_COUNTER /= 2 + 1
+    print(f"Need {LOOP_COUNTER} more sorc pts ({LOOP_COUNTER+Current_sorc_pts} total)")
+    
+    sleep(5)
+    start_time = time()
+    
+    # -----------------
+    
+    # get all necessary sorc pts
+    msg = f"Getting {LOOP_COUNTER} sorc pts ({Target_sorc_pts} pt targ + {Spell_1_pts} pts for lvl 1 spellslots + {Spell_2_pts} pts for lvl 2 spellslots"
+    if unlocked_spellslots_3:
+        msg += f" + {Spell_3_pts} pts for lvl 3 spellslots"
+        if unlocked_spellslots_4:
+            msg += f"  {Spell_4_pts} pts for lvl 4 spellslots"
+            if unlocked_spellslots_5:
+                msg += f" + {Spell_5_pts} pts for lvl 5 spellslots"
+    print(msg+")...")
+    
+    for _ in range(LOOP_COUNTER):
+        activate_equipment(using_shield)
+    
+    # expand spell slots
+    spellslot_data = {
+        1: {"target": Target_spellslots_1, "unlocked": True},
+        2: {"target": Target_spellslots_2, "unlocked": True},
+        3: {"target": Target_spellslots_3, "unlocked": unlocked_spellslots_3},
+        4: {"target": Target_spellslots_4, "unlocked": unlocked_spellslots_4},
+        5: {"target": Target_spellslots_5, "unlocked": unlocked_spellslots_5},
+    }
+
+    # loop through levels and create spellslots if unlocked
+    for level, data in spellslot_data.items():
+        if data["unlocked"]:  # check if the level is unlocked
+            print(f"Creating {data['target']} lvl {level} spellslots...")
+            for _ in range(data["target"]):
+                create_spellslot(level)
+    
+    stop_time = time()
+    print(f"Macro completed in {stop_time-start_time:.2f}s")
 
 # -------------------------------------------------------------------------------------------------
 
@@ -119,128 +228,21 @@ def activate_equipment(using_shield: bool):
     if using_shield:
         create_sorcery_pts(1)
     else:
-        create_sorcery_pts(2) # todo: amulet pts count as 2
+        create_sorcery_pts(2)
     
     #print("Unequipping item")
     move_and_click(1285,1330)
     
     sleep(.025)
-
-def macro():
-    """function def."""
     
-    #############################################
-    # CHANGE ALL CURRENT VALUES HERE
-    
-    using_shield = True           # set to false if using the amulet
-    unlocked_spellslots_3 = True # set any of these to true if they are unlocked and you want 
-    unlocked_spellslots_4 = True # them expanded
-    unlocked_spellslots_5 = True
-    
-    # UPDATE THESE VALUES TO REFLECT YOUR CURRENT SPELL SLOTS
-    # Note that this number should be considered after all sorc pts are gathered (since we are 
-    # using first and second level spell slots) 
-    
-    if using_shield: # if we use the shield, we will use all first-level spell slots
-        Current_spellslots_1 = 0 # leave this 0
-        Current_spellslots_2 = 20
-    else: # likewise for the amulet, we will use all second-level spell slots
-        Current_spellslots_1 = 4
-        Current_spellslots_2 = 0 # leave this 0
-    Current_spellslots_3 = 3
-    Current_spellslots_4 = 3
-    Current_spellslots_5 = 1
-    Current_sorc_pts = 51
-    
-    # THESE ARE THE TARGET VALUES YOU WANT
-    Target_spellslots_1 = 20 # e.g. I want 20 first-level spell slots
-    Target_spellslots_2 = 20
-    Target_spellslots_3 = 10
-    Target_spellslots_4 = 10
-    Target_spellslots_5 = 5
-    Target_sorc_pts = 30
-    #############################################
-
-    # SETUP: 
-    # TARGET GOAL -> need:
-    # I: 20   -> +16 slots (32 pts) [x2]
-    Target_spellslots_1 -= Current_spellslots_1
-    Spell_1_pts = Target_spellslots_1*2
-    # Note: if we're using the shield, all of these will be depleted so we need to create all of them
-
-    # II: 20  -> +17 slots (51 pts) [x3]
-    Target_spellslots_2 -= Current_spellslots_2
-    Spell_2_pts = Target_spellslots_2*3
-    # Note: if we're using the amulet, all of these will be depleted so we need to create all of them
-    Target_sorc_pts -= Current_sorc_pts 
-
-    # the loop counter needs to be the total amount of sorcery points needed
-    LOOP_COUNTER: int = Spell_1_pts+Spell_2_pts + Target_sorc_pts
-    # III: 10 -> +7 slots (35 pts) [x5]
-    if unlocked_spellslots_3:
-        Target_spellslots_3 -= Current_spellslots_3
-        Spell_3_pts = Target_spellslots_3*5
-        LOOP_COUNTER += Spell_3_pts
-        
-        # IV: 10  -> +7 slots (42 pts) [x6]
-        if unlocked_spellslots_4:
-            Target_spellslots_4 -= Current_spellslots_4
-            Spell_4_pts = Target_spellslots_4*6
-            LOOP_COUNTER += Spell_4_pts
-            
-            # V: 5    -> +4 slots (28 pts) [x7]
-            if unlocked_spellslots_5:
-                Target_spellslots_5 -= Current_spellslots_5
-                Spell_5_pts = Target_spellslots_5*7
-                LOOP_COUNTER += Spell_5_pts
-
-    print(f"Need {LOOP_COUNTER} more sorc pts ({LOOP_COUNTER+Current_sorc_pts} total)")
-    
-    sleep(5)
-    start_time = time()
-    
-    # -----------------
-    
-    # get all necessary sorc pts
-    msg = f"Getting {LOOP_COUNTER} sorc pts ({Target_sorc_pts} pt targ + {Spell_1_pts} pts for lvl 1 spellslots + {Spell_2_pts} pts for lvl 2 spellslots"
-    if unlocked_spellslots_3:
-        msg += f" + {Spell_3_pts} pts for lvl 3 spellslots"
-        if unlocked_spellslots_4:
-            msg += f"  {Spell_4_pts} pts for lvl 4 spellslots"
-            if unlocked_spellslots_5:
-                msg += f" + {Spell_5_pts} pts for lvl 5 spellslots"
-    print(msg+")...")
-    
-    for _ in range(LOOP_COUNTER):
-        activate_equipment(using_shield)
-    
-    # expand spell slots
-    spellslot_data = {
-        1: {"target": Target_spellslots_1, "unlocked": True},
-        2: {"target": Target_spellslots_2, "unlocked": True},
-        3: {"target": Target_spellslots_3, "unlocked": unlocked_spellslots_3},
-        4: {"target": Target_spellslots_4, "unlocked": unlocked_spellslots_4},
-        5: {"target": Target_spellslots_5, "unlocked": unlocked_spellslots_5},
-    }
-
-    # loop through levels and create spellslots if unlocked
-    for level, data in spellslot_data.items():
-        if data["unlocked"]:  # check if the level is unlocked
-            print(f"Creating {data['target']} lvl {level} spellslots...")
-            for _ in range(data["target"]):
-                create_spellslot(level)
-    
-    stop_time = time()
-    print(f"Macro completed in {stop_time-start_time:.2f}s")
-
 
 def main():
     #time.sleep(5)
     # Bind the macro to a hotkey (e.g., CTRL+ALT+M)
-    keyboard.add_hotkey('shift+r', macro)
+    add_hotkey('shift+r', macro)
     
     # Keep the script running to listen for the hotkey
-    keyboard.wait('esc')  # Exit the script by pressing ESC
+    wait('esc')  # Exit the script by pressing ESC
 
 
 if __name__ == "__main__":
