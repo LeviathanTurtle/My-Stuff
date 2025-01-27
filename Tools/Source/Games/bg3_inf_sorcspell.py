ada441a4bc16400ce5987614946bcd9b40d2a857
LeviathanTurtle <wbwads@gmail.com>
2025-01-27 11:32:46 -0500

    sixth level func, updated coords, est runtime, updated coord logic, reorg

--- Tools/Source/Games/bg3_inf_sorcspell.py
+++ Tools/Source/Games/bg3_inf_sorcspell.py
@@ -6,143 +6,129 @@
 # 
 # [notes]
 # todo PROBLEMS:
-# - coords assume level 5 unlocked
-# - check that two-handed weapons are unequipped
-# - estimated runtime
+# - check conv for spellslot lvl 6
+# - test with other FPS values
+# - add photos in readme doc
+# - literally test it
 # 
-
-
-# level 1:
-# - 1305 1250
-# level 2:
-# - 1275 1250
-# - 1335 1250
-# level 3:
-# - 1250 1250
-# - 1305 1250
-# - 1365 1250
-# level 4:
-# - 1215 1250
-# - 1275 1250
-# - 1335 1250
-# - 1395 1250
-# level 5:
-# - 1190 1250
-# - 1250 1250
-# - 1305 1250
-# - 1365 1250
-# - 1425 1250
-# level 6:
-# - 1160 1250
-# - 1220 1250
-# - 1280 1250
-# - 1335 1250
-# - 1395 1250
-# - 1450 1250
-
 
 from keyboard import add_hotkey, wait
 from pydirectinput import moveTo, mouseDown, mouseUp
 from time import sleep, time
 
+# pyautogui uses this, but not pydirectinput
 #MOUSE_SPEED = 1 
-# ^ pyautogui uses this, but not pydirectinput
-
 # the length of time to sleep between inputs (in seconds)
 SLEEP_DURATION = .05
 
+#################################################
+# CHANGE ONLY THESE VALUES HERE
+
+# set any of these to true if they are unlocked and you want them expanded
+unlocked_spellslots_2 = True
+unlocked_spellslots_3 = True
+unlocked_spellslots_4 = False 
+unlocked_spellslots_5 = False
+unlocked_spellslots_6 = False
+# set this to false if using the amulet
+using_shield = True
+
+# UPDATE THESE VALUES TO REFLECT YOUR CURRENT SPELL SLOTS
+# Note that the current number of spellslots for level 1 should be 0 if using the shield, likewise
+# for level 2 if using the amulet
+if using_shield:
+    Current_spellslots_1 = 0 # leave this 0
+    Current_spellslots_2 = 3
+else:
+    Current_spellslots_1 = 4
+    Current_spellslots_2 = 0 # leave this 0
+Current_spellslots_3 = 3
+Current_spellslots_4 = 1
+Current_spellslots_5 = 1
+Current_spellslots_6 = 1
+Current_sorc_pts = 5
+
+# THESE ARE THE TARGET VALUES YOU WANT
+Target_spellslots_1 = 15 
+Target_spellslots_2 = 10
+Target_spellslots_3 = 10
+Target_spellslots_4 = 5
+Target_spellslots_5 = 5
+Target_spellslots_6 = 5
+Target_sorc_pts = 30
+
+#################################################
+
+
 def macro():
     """function def."""
-    
-    #############################################
-    # CHANGE ALL CURRENT VALUES HERE
-    
-    using_shield = True           # set to false if using the amulet
-    unlocked_spellslots_2 = True  # set any of these to true if they are unlocked and you want 
-    unlocked_spellslots_3 = True  # them expanded
-    unlocked_spellslots_4 = True 
-    unlocked_spellslots_5 = False
-    
-    # UPDATE THESE VALUES TO REFLECT YOUR CURRENT SPELL SLOTS
-    # Note that this number should be considered after all sorc pts are gathered (since we are 
-    # using first and second level spell slots) 
-    
-    # todo: swap these clauses
-    if using_shield: # if we use the shield, we will use all first-level spell slots
-        Current_spellslots_1 = 0 # leave this 0
-        Current_spellslots_2 = 3
-    else: # likewise for the amulet, we will use all second-level spell slots
-        Current_spellslots_1 = 4
-        Current_spellslots_2 = 0 # leave this 0
-    Current_spellslots_3 = 3
-    Current_spellslots_4 = 1
-    Current_spellslots_5 = 1
-    Current_sorc_pts = 5
-    
-    # THESE ARE THE TARGET VALUES YOU WANT
-    Target_spellslots_1 = 15 # e.g. I want 20 first-level spell slots
-    Target_spellslots_2 = 10
-    Target_spellslots_3 = 10
-    Target_spellslots_4 = 5
-    Target_spellslots_5 = 5
-    Target_sorc_pts = 30
-    #############################################
 
-    # SETUP: 
-    # TARGET GOAL -> need:
-    # I: 20   -> +16 slots (32 pts) [x2]
+    # SETUP:
+    # LEVEL -> needed sorc pts per level:
+    # I -> [x2]
     Target_spellslots_1 -= Current_spellslots_1
     Spell_1_pts = Target_spellslots_1*2
-    # Note: if we're using the shield, all of these will be depleted so we need to create all of them
 
-    # II: 20  -> +17 slots (51 pts) [x3]
+    # II -> [x3]
     Target_spellslots_2 -= Current_spellslots_2
     Spell_2_pts = Target_spellslots_2*3
-    # Note: if we're using the amulet, all of these will be depleted so we need to create all of them
     Target_sorc_pts -= Current_sorc_pts 
 
     # the loop counter needs to be the total amount of sorcery points needed
     LOOP_COUNTER: int = Spell_1_pts+Spell_2_pts + Target_sorc_pts
-    # III: 10 -> +7 slots (35 pts) [x5]
+    # III -> [x5]
     if unlocked_spellslots_3:
         Target_spellslots_3 -= Current_spellslots_3
         Spell_3_pts = Target_spellslots_3*5
         LOOP_COUNTER += Spell_3_pts
         
-        # IV: 10  -> +7 slots (42 pts) [x6]
+        # IV -> [x6]
         if unlocked_spellslots_4:
             Target_spellslots_4 -= Current_spellslots_4
             Spell_4_pts = Target_spellslots_4*6
             LOOP_COUNTER += Spell_4_pts
             
-            # V: 5    -> +4 slots (28 pts) [x7]
+            # V -> [x7]
             if unlocked_spellslots_5:
                 Target_spellslots_5 -= Current_spellslots_5
                 Spell_5_pts = Target_spellslots_5*7
                 LOOP_COUNTER += Spell_5_pts
+                
+                # VI -> [x]
+                #if unlocked_spellslots_6:
+                #    Target_spellslots_6 -= Current_spellslots_6
+                #    Spell_6_pts = Target_spellslots_6*_
+                #    LOOP_COUNTER += Spell_6_pts
 
+    # if we are using the amulet, update the loop counter to use half the required iterations
     if not using_shield:
-        LOOP_COUNTER /= 2 + 1
+        if LOOP_COUNTER % 2 != 0:
+            LOOP_COUNTER //= 2 + 1 # if it is odd, offset by 1 so we are over requirement
+        else: LOOP_COUNTER /= 2
+        
     print(f"Need {LOOP_COUNTER} more sorc pts ({LOOP_COUNTER+Current_sorc_pts} total)")
-    print(f"Estimated runtime: {LOOP_COUNTER} ({LOOP_COUNTER} min)")
+    est_runtime = (0.1 + (SLEEP_DURATION/2)) * LOOP_COUNTER * 8
+    print(f"Estimated runtime: {est_runtime:.2f}s ({est_runtime/60:.2f} min)")
     
     sleep(5)
     start_time = time()
     
-    # -----------------
+    # -------------------------------------------
     
     # get all necessary sorc pts
     msg = f"Getting {LOOP_COUNTER} sorc pts ({Target_sorc_pts} pt targ + {Spell_1_pts} pts for lvl 1 spellslots + {Spell_2_pts} pts for lvl 2 spellslots"
     if unlocked_spellslots_3:
         msg += f" + {Spell_3_pts} pts for lvl 3 spellslots"
         if unlocked_spellslots_4:
-            msg += f"  {Spell_4_pts} pts for lvl 4 spellslots"
+            msg += f" + {Spell_4_pts} pts for lvl 4 spellslots"
             if unlocked_spellslots_5:
                 msg += f" + {Spell_5_pts} pts for lvl 5 spellslots"
+                #if unlocked_spellslots_6:
+                #    msg += f" + {Spell_6_pts} pts for lvl 6 spellslots"
     print(msg+")...")
     
     for _ in range(LOOP_COUNTER):
-        activate_equipment(using_shield)
+        activate_equipment(using_shield) # (0.1 + SLEEP_DURATION/2) * 5 * LOOP_DURATION
     
     # expand spell slots
     spellslot_data = {
@@ -151,6 +137,7 @@ def macro():
         3: {"target": Target_spellslots_3, "unlocked": unlocked_spellslots_3},
         4: {"target": Target_spellslots_4, "unlocked": unlocked_spellslots_4},
         5: {"target": Target_spellslots_5, "unlocked": unlocked_spellslots_5},
+        6: {"target": Target_spellslots_6, "unlocked": unlocked_spellslots_6},
     }
 
     # loop through levels and create spellslots if unlocked
@@ -158,7 +145,7 @@ def macro():
         if data["unlocked"]:  # check if the level is unlocked
             print(f"Creating {data['target']} lvl {level} spellslots...")
             for _ in range(data["target"]):
-                create_spellslot(level)
+                create_spellslot(level) # (0.1 + SLEEP_DURATION/2) * 3 * LOOP_DURATION
     
     stop_time = time()
     print(f"Macro completed in {stop_time-start_time:.2f}s")
@@ -174,8 +161,6 @@ def move_and_click(coord_x: int, coord_y: int):
     mouseDown()
     sleep(0.1) # hold for 100ms
     mouseUp()
-    
-    #sleep(SLEEP_DURATION)
 
 def select_metamagic(type: str):
     """Select the metamagic icons in the UI."""
@@ -187,19 +172,95 @@ def select_metamagic(type: str):
     else:
         pass # error
 
+# level 1:
+# - 1305 1250
+# level 2:
+# - 1275 1250
+# - 1335 1250
+# level 3:
+# - 1250 1250
+# - 1305 1250
+# - 1365 1250
+# level 4:
+# - 1215 1250
+# - 1275 1250
+# - 1335 1250
+# - 1395 1250
+
+# level 5:
+# - 1190 1250
+# - 1250 1250
+# - 1305 1250
+# - 1365 1250
+# - 1425 1250
+
+# level 6:
+# - 1160 1250
+# - 1220 1250
+# - 1280 1250
+# - 1335 1250
+# - 1395 1250
+# - 1450 1250
+
 def create_sorcery_pts(spellslot_level: int):
     """function def."""
     
-    #print(f"Creating {spellslot_level} sorcery pt(s)...")
-    
     spellslot_level_y = 1250
-    match (spellslot_level): # this assumes all 5 are unlocked
-        case 1:
-            # 1190 1250
-            spellslot_level_x = 1250
-        case 2:
-            # 1250 1250
-            spellslot_level_x = 1305
+    spellslot_level_x = 1305
+    if unlocked_spellslots_2:
+        match (spellslot_level):
+            case 1:
+                spellslot_level_x = 1275
+            case 2:
+                spellslot_level_x = 1335
+        
+        if unlocked_spellslots_3:
+            match (spellslot_level):
+                case 1:
+                    spellslot_level_x = 1250
+                case 2:
+                    spellslot_level_x = 1305
+                case 3:
+                    spellslot_level_x = 1365
+            
+            if unlocked_spellslots_4:
+                match (spellslot_level):
+                    case 1:
+                        spellslot_level_x = 1215
+                    case 2:
+                        spellslot_level_x = 1275
+                    case 3:
+                        spellslot_level_x = 1335
+                    case 4:
+                        spellslot_level_x = 1395
+                
+                if unlocked_spellslots_5:
+                    match (spellslot_level):
+                        case 1:
+                            spellslot_level_x = 1190
+                        case 2:
+                            spellslot_level_x = 1250
+                        case 3:
+                            spellslot_level_x = 1305
+                        case 4:
+                            spellslot_level_x = 1365
+                        case 5:
+                            spellslot_level_x = 1425
+                    
+                    if unlocked_spellslots_6:
+                        match (spellslot_level):
+                            case 1:
+                                spellslot_level_x = 1160
+                            case 2:
+                                spellslot_level_x = 1220
+                            case 3:
+                                spellslot_level_x = 1280
+                            case 4:
+                                spellslot_level_x = 1335
+                            case 5:
+                                spellslot_level_x = 1395
+                            case 6:
+                                spellslot_level_x = 1450
 
     select_metamagic("SORCPTS")
 
@@ -209,29 +270,67 @@ def create_sorcery_pts(spellslot_level: int):
     # cast (mouse pos 1200 1050)
     move_and_click(1200,1050)
     
-    #print(f"Created {spellslot_level} sorcery pt(s)")
     sleep(2)
 
 def create_spellslot(spellslot_level: int):
     """function def."""
     
     spellslot_level_y = 1250
-    match (spellslot_level):
-        case 1:
-            # 1190 1250
-            spellslot_level_x = 1250
-        case 2:
-            # 1250 1250
-            spellslot_level_x = 1305
-        case 3:
-            # 1300 1250 
-            spellslot_level_x = 1365
-        case 4:
-            # 1365 1250
-            spellslot_level_x = 1365
-        case 5:
-            # 1425 1250
-            spellslot_level_x = 1425
+    spellslot_level_x = 1305
+    if unlocked_spellslots_2:
+        match (spellslot_level):
+            case 1:
+                spellslot_level_x = 1275
+            case 2:
+                spellslot_level_x = 1335
+        
+        if unlocked_spellslots_3:
+            match (spellslot_level):
+                case 1:
+                    spellslot_level_x = 1250
+                case 2:
+                    spellslot_level_x = 1305
+                case 3:
+                    spellslot_level_x = 1365
+            
+            if unlocked_spellslots_4:
+                match (spellslot_level):
+                    case 1:
+                        spellslot_level_x = 1215
+                    case 2:
+                        spellslot_level_x = 1275
+                    case 3:
+                        spellslot_level_x = 1335
+                    case 4:
+                        spellslot_level_x = 1395
+                
+                if unlocked_spellslots_5:
+                    match (spellslot_level):
+                        case 1:
+                            spellslot_level_x = 1190
+                        case 2:
+                            spellslot_level_x = 1250
+                        case 3:
+                            spellslot_level_x = 1305
+                        case 4:
+                            spellslot_level_x = 1365
+                        case 5:
+                            spellslot_level_x = 1425
+                    
+                    if unlocked_spellslots_6:
+                        match (spellslot_level):
+                            case 1:
+                                spellslot_level_x = 1160
+                            case 2:
+                                spellslot_level_x = 1220
+                            case 3:
+                                spellslot_level_x = 1280
+                            case 4:
+                                spellslot_level_x = 1335
+                            case 5:
+                                spellslot_level_x = 1395
+                            case 6:
+                                spellslot_level_x = 1450
     
     # goto spellslot icon (mouse pos 1745 1275)
     select_metamagic("SPELLSLOTS")
@@ -274,7 +373,6 @@ def activate_equipment(using_shield: bool):
     
 
 def main():
-    #time.sleep(5)
     # Bind the macro to a hotkey (e.g., CTRL+ALT+M)
     add_hotkey('shift+r', macro)
     
